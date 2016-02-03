19e253091ceead38de4b4ddc6aa686101155735c
hana <hana.ballet0101@gmail.com>
2016-02-03 21:55:44 +0900

    first commit

--- CococaPodsApp/ViewController.swift
+++ CococaPodsApp/ViewController.swift
@@ -7,19 +7,31 @@
 //
 
 import UIKit
+import PKHUD
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        PKHUD.sharedHUD.contentView = PKHUDSuccessView()
+        
+        //
+        PKHUD.sharedHUD.show()
+        
+        //
+        PKHUD.sharedHUD.hide(afterDelay: 2.0)
+        //Do any additional setup after loading the view, typically from a nib.
+        
+    }
+   
 }
 
